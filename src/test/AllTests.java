321b005b2a087d8a9adde694422e11091f270ecf
sjud9227 <swdunnin@asu.edu>
2015-05-08 18:06:02 -0700

    Update AllTests.java

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -1,18 +1,14 @@
 /**
- * 
- */
-/**
  * @author SWD
- *
  */
+ 
 package test;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ TestBoard.class, BoardTest.class, TestConsolePlayer.class, TestGame.class })
-public class AllTests {
 
+public class AllTests {
 }
