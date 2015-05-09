3d45b6c9d44928eae2db40c9de70cd7546e2f1a3
sjud9227 <swdunnin@asu.edu>
2015-05-08 19:21:11 -0700

    Update ConsolePlayer.java

--- src/connect/four/player/ConsolePlayer.java
+++ src/connect/four/player/ConsolePlayer.java
@@ -1,9 +1,11 @@
 
 package connect.four.player;
+import connect.four.board.ColumnFullException;
 import connect.four.board.ReadableBoard;
 import connect.four.board.ReadWritableBoard;
 import connect.four.Game;
 import connect.four.ScoreChart;
+
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,33 +27,10 @@ public class ConsolePlayer implements Player, ScoreChart.Listener {
     }
 
     @Override public void gameOver(Player winner, ScoreChart scores, ReadableBoard board) {
-        System.out.println(m_name + (winner == this ? " won." : " lost."));
-        dumpBoard(board);
-        System.out.println(m_name + ": " + scores.getScore(this));
-        
-        //Added to make the console play less confusing after a game has ended.  
-        //added an option to quit game or play again.
-        System.out.println();
-        System.out.println("Would you like to play again?");
-        System.out.println("(Enter 0 for quit and 1 for play again)");
-        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-        int x = -1;
-        while (x < 0 || x > 1) {
-            try {
-                System.out.print("Enter your selection: ");
-                x = Integer.parseInt(stdin.readLine());
-            } catch (IOException e) {
-                // loop again.
-            } catch (NumberFormatException e) {
-                // loop again.
-            }
-        }
-        if(x == 0)
-            System.exit(0);
-        else
-        	System.out.println("Playing again!");
-    //end aded code ======================================================    	
+    	 System.out.println(m_name + (winner == this ? " won." : " lost."));  
+         System.out.println(m_name + ": " + scores.getScore(this));           
     }
+    
 
     @Override public void performPlay(ReadWritableBoard board) {
         int width = board.getWidth();
@@ -62,37 +41,62 @@ public class ConsolePlayer implements Player, ScoreChart.Listener {
 
         BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         int x = 0;
-        while (x < 1 || x > width) {
-            try {
-                System.out.print("Enter the column you want to play in: ");
-                x = Integer.parseInt(stdin.readLine());
-            } catch (IOException e) {
-                // loop again.
-            } catch (NumberFormatException e) {
-                // loop again.
-            }
+        boolean Invalid = true;
+        while(Invalid)
+        {
+        	while (x < 1 || x > width) {
+        		try {  
+        			
+        			System.out.print("Enter the column you want to play in: (0 to quit): ");
+        			x = Integer.parseInt(stdin.readLine());
+        			if( x==0){
+        				System.exit(0);
+        			}
+        		} catch (IOException e) {
+        			// loop again.
+        		} catch (NumberFormatException e) {
+        			// loop again.
+        		} 
+        	}
+        	if(board.getColumnHeight(x-1) == height)
+        	{
+        		Invalid = true;
+        		System.out.println("Can't go there its full.");
+        		x = 0;
+        	}
+        	else
+        	{
+        		Invalid = false;
+        	}
         }
-
         board.play(x-1, this);
     }
 
+    /**
+     * Added code to change the cosmetic look of the console board,
+     * 
+     * */
     private void dumpBoard(ReadableBoard board) {
         int width = board.getWidth();
         int height = board.getHeight();
 
-        System.out.println("@ is you, X is the other player, and O is empty.");
+        System.out.println("@ is you, X is the other player, and - is empty.");
         for (int i = height-1; i != -1; --i) {
             for (int j = 0; j != width; ++j) {
                 Player played = board.whoPlayed(j, i);
                 System.out.print(
-                    played == this ? "@" :
-                    played == null ? "O" : "X"
+                    played == this ? "@ " :
+                    played == null ? "- " : "X "
                 );
             }
             System.out.println();
         }
+        for (int i = 0; i != width+6; ++i) {
+            System.out.print("=");
+        }
+        System.out.println();
         for (int i = 0; i != width; ++i) {
-            System.out.print(i+1);
+            System.out.print(i+1 + " ");
         }
         System.out.println();
     }
