51bf34d43ca05d34442f3e748ac7cb58425818dc
sjud9227 <swdunnin@asu.edu>
2015-05-08 19:15:31 -0700

    Update ColumnFullException.java

--- src/connect/four/board/ColumnFullException.java
+++ src/connect/four/board/ColumnFullException.java
@@ -2,11 +2,12 @@
 package connect.four.board;
 
 
+@SuppressWarnings("serial")
 public class ColumnFullException extends IndexOutOfBoundsException {
     public ColumnFullException(String message) {
-	super(message);
+	    super(message);
     }
     public ColumnFullException() {
-	super("Played in a full column.");
+    	super("Played in a full column.");
     }
 }
